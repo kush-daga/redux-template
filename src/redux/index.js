0e7f80edfa9555e9607ec038b26d498839920db5
Kush Daga <40840079+kush-daga@users.noreply.github.com>
2020-08-24 21:17:49 +0530

    Update index.js

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,13 +1,2 @@
 export { buyCake } from "./cake/cakeActions";
 
-// im not streeaming lol
-// its just video cam being changed
-// to screen share
-// applied a hack
-// if i stream, u can only see my
-//one window, not full screen thats
-//why had to apply this hack
-//Cant speak sorry :(
-
-//CREATING A TEMPLATE FOR MY
-//REDUX WORKFLOW
